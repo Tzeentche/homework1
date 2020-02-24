eb768229c48d4f8483ec9e7fba946b9b52891ac2
Tzeentche <ilyasugako87@gmail.com>
2020-02-24 07:58:26 +0300

    Add task 2.

--- src/task1/Hello.java
+++ src/task1/Hello.java
@@ -1,7 +1,9 @@
+package task1;
+
 public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        System.out.println("task1.Hello, world!");
     }
 }
