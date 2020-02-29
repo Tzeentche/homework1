66821cb4be7aa7ac09d2a91572350efce05d99b8
Tzeentche <ilyasugako87@gmail.com>
2020-02-29 16:13:29 +0300

    Started to work with task 10.

---
+++ src/task10/HasInteger.java
@@ -0,0 +1,11 @@
+package task10;
+
+//      Имеется целове число. Следует посчитать сумму цифр данного числа.
+
+public class HasInteger {
+
+    public static void main(String[] args) {
+
+
+    }
+}
