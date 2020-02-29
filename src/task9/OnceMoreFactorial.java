c28f4e80c6b9e7c98e54b87f8a26f0926f22abd6
Tzeentche <ilyasugako87@gmail.com>
2020-02-29 16:13:17 +0300

    Work with task 9 was finished.

--- src/task9/OnceMoreFactorial.java
+++ src/task9/OnceMoreFactorial.java
@@ -8,6 +8,14 @@ public class OnceMoreFactorial {
 
     public static void main(String[] args) {
 
-       
+        long fac = 1;
+        int i = 1;
+        while(i < 59) {
+
+            fac *= i;
+            i++;
+        }
+
+        System.out.println("Factorial is: " + fac);
     }
 }
