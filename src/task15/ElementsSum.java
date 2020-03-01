9b6e27bb8e6de687a78210c0ba790c8f0da73c6c
Tzeentche <ilyasugako87@gmail.com>
2020-03-02 00:14:57 +0300

    Finished work with task 15.

--- src/task15/ElementsSum.java
+++ src/task15/ElementsSum.java
@@ -12,5 +12,66 @@ public class ElementsSum {
         elementsSum.giveMeSumm();
     }
 
-    
+    void giveMeSumm() {
+
+        System.out.println("Summ of array's elements is " + summContainer());
+    }
+
+    int summContainer() {
+
+        int container = 0, max = 0, min = 0;
+        int[] elementsArray = usersInput();
+
+        try{
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Now please, enter integer MIN summ index:");
+            min = scn.nextInt();
+
+            System.out.println("Now please, enter integer MAX summ index:");
+            max = scn.nextInt();
+            scn.close();
+
+        } catch (InputMismatchException ex) {
+
+            System.out.println("Please< next time enter correct value!");
+        }
+
+        for (int i = min - 1; i < max; i++) {
+            container += elementsArray[i];
+        }
+        return container;
+    }
+
+    int[] usersInput() {
+
+        int size = 0;
+
+        try{
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Please, enter integer size of array:");
+            size = scn.nextInt();
+
+        } catch (InputMismatchException ex) {
+
+            System.out.println("Please< next time enter correct value!");
+        }
+
+        int[] array = arrayFilling(size);
+        return array;
+    }
+
+    int[] arrayFilling(int size) {
+
+        int[] fullArray = new int[size];
+
+        System.out.println("Now your array looks like this: ");
+        for(int i = 0; i < size; i++) {
+
+            fullArray[i] = (int)(Math.random() * 10);
+            System.out.print(fullArray[i] + " ");
+        }
+        System.out.println("...and contains " + size + " elements.");
+
+        return fullArray;
+    }
 }
