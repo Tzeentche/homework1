b8b7fbc14836f0c016ce95d9034881300a05f6f1
Tzeentche <ilyasugako87@gmail.com>
2020-03-01 09:26:25 +0300

    Started to work with task 11.

---
+++ src/task11/SimpleNum.java
@@ -0,0 +1,13 @@
+package task11;
+
+//      Имеется целое число, определить, является ли это число простым, т.е. делится без остатка только на 1 и себя
+
+import java.util.*;
+
+public class SimpleNum {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
