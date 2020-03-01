8044c3e9fa252e6baaaa17c13db05ea91d12f637
Tzeentche <ilyasugako87@gmail.com>
2020-03-01 22:44:22 +0300

    Nothing to change.

--- src/task9/OnceMoreFactorial.java
+++ src/task9/OnceMoreFactorial.java
@@ -2,8 +2,6 @@ package task9;
 
 //      Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а произведение, т.е. факториал числа
 
-import java.util.*;
-
 public class OnceMoreFactorial {
 
     public static void main(String[] args) {
