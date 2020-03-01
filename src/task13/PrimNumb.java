52a9005bca4e41d5efdfe0545eb6e3f272162bae
Tzeentche <ilyasugako87@gmail.com>
2020-03-01 22:44:04 +0300

    Create logic and methods to task 13.

---
+++ src/task13/PrimNumb.java
@@ -0,0 +1,48 @@
+package task13;
+
+//        Решить задание на поиск простых чисел с использованием break.
+
+import java.util.*;
+
+public class PrimNumb {
+
+    static int container = 1;
+
+    public static void main(String[] args) {
+
+        userInput();
+
+        if(revisor()) {
+            System.out.println("Congratulations! Your entered number " + container + " is Prime!");
+        } else {
+            System.out.println("Unfortunately, this number isn't Prime.");
+        }
+    }
+
+    static int userInput() {
+
+        try {
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Please, enter your number:");
+            container = scn.nextInt();
+            scn.close();
+
+        } catch (InputMismatchException ex) {
+            System.out.println("What do you think about? Enter Integer value, please!");
+        }
+        return container;
+    }
+
+    static boolean revisor() {
+
+        int boofer = 0;
+        for (int i = 1; i <= container; i++) {
+
+            if ((container % i) == 0) {
+                boofer++;
+            }
+        }
+
+        return (boofer < 3) ? true : false;
+    }
+}
