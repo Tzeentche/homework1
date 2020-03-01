578970e465520b1905443f617bf23881a00e1f36
Tzeentche <ilyasugako87@gmail.com>
2020-03-01 17:53:43 +0300

    Started work with task 13.

---
+++ src/task13/primNumb.java
@@ -0,0 +1,10 @@
+package task13;
+
+//        Решить задание на поиск простых числе с использованием break.
+
+public class primNumb {
+
+    public static void main(String[] args) {
+        
+    }
+}
