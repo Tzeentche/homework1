9af37dcd8509041adfd869ddb6a5b057753ea47c
Tzeentche <ilyasugako87@gmail.com>
2020-03-02 00:14:36 +0300

    Started work with task 14.

---
+++ src/task15/ElementsSum.java
@@ -0,0 +1,16 @@
+package task15;
+
+//      Определите сумму элементов массива, расположенных между минимальным и максимальным значениями.
+
+import java.util.*;
+
+public class ElementsSum {
+
+    public static void main(String[] args) {
+
+        ElementsSum elementsSum = new ElementsSum();
+        elementsSum.giveMeSumm();
+    }
+
+    
+}
