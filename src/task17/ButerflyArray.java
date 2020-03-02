22ae2822fbaa42b244806c615db58116c3b7c906
Tzeentche <ilyasugako87@gmail.com>
2020-03-02 11:48:53 +0300

    Started development task 17.

---
+++ src/task17/ButerflyArray.java
@@ -0,0 +1,16 @@
+package task17;
+
+//      Создать двумерный квадратный массив, и заполнить его "бабочкой"
+//      1 1 1 1 1
+//      0 1 1 1 0
+//      0 0 1 0 0
+//      0 1 1 1 0
+//      1 1 1 1 1
+
+public class ButerflyArray {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
