5ed345c32d0da1a764e2fe8f1e4dedc2c982cb23
Tzeentche <ilyasugako87@gmail.com>
2020-03-04 00:47:31 +0300

    Finished developing code for 18th task.

--- src/task18/PunctuationMarks.java
+++ src/task18/PunctuationMarks.java
@@ -1,6 +1,7 @@
 package task18;
 
 //      Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
+//      Рекомендуемый текст: "Окна; во всех. корпусах были, ярко?? освещены? И оттого! На громадном, дворе казалось, что очень темно.".
 
 import java.util.*;
 
@@ -9,30 +10,44 @@ public class PunctuationMarks {
     public static void main(String[] args) {
 
         PunctuationMarks punctuationMarks = new PunctuationMarks();
-        punctuationMarks.outputResult();
 
+        String someText = punctuationMarks.usersText();
+        String[] textArray = punctuationMarks.textTerminator(someText);
+        int quantity = punctuationMarks.counter(textArray);
+        punctuationMarks.outputResult(quantity);
     }
 
     String usersText() {
 
-        return "";
+        String usersTextIs = "";
+
+        try {
+            System.out.println("Please, enter some text with punctuation marks:");
+            Scanner scn = new Scanner(System.in);
+            usersTextIs = scn.nextLine();
+            scn.close();
+        } catch(InputMismatchException ex) {
+
+            System.out.println("Please, try again/ And next time be more carefully!");
+        }
+
+        return usersTextIs;
     }
 
-    String[] textTerminator() {
+    String[] textTerminator(String someText) {
 
-
-        return;
+        String[] textArray = someText.split("[.,;+!?]");
+        return textArray;
     }
 
-    int counter() {
+    int counter(String[] punctuations) {
 
-        
-        return 0;
+        int counter = punctuations.length;
+        return counter;
     }
 
-    void outputResult() {
+    void outputResult(int punctuationsQuantity) {
 
-        int resultIs = counter();
-        System.out.println("There are " + resultIs + " punctuation marks in your text");
+        System.out.println("There are " + punctuationsQuantity + " punctuation marks in your text");
     }
 }
