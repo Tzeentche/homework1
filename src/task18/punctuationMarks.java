f1ec79a45c8d3fab84514931be162eea9fd07b5e
Tzeentche <ilyasugako87@gmail.com>
2020-03-03 23:26:21 +0300

    Started to developing 18th task.

---
+++ src/task18/punctuationMarks.java
@@ -0,0 +1,13 @@
+package task18;
+
+//      Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
+
+import java.util.*;
+
+public class punctuationMarks {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
