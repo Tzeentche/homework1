3473631e69ad01940730b53a5da382419148944b
Tzeentche <ilyasugako87@gmail.com>
2020-03-03 23:42:06 +0300

    Add methods and logic to 18th task.

---
+++ src/task18/PunctuationMarks.java
@@ -0,0 +1,38 @@
+package task18;
+
+//      Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
+
+import java.util.*;
+
+public class PunctuationMarks {
+
+    public static void main(String[] args) {
+
+        PunctuationMarks punctuationMarks = new PunctuationMarks();
+        punctuationMarks.outputResult();
+
+    }
+
+    String usersText() {
+
+        return "";
+    }
+
+    String[] textTerminator() {
+
+
+        return;
+    }
+
+    int counter() {
+
+        
+        return 0;
+    }
+
+    void outputResult() {
+
+        int resultIs = counter();
+        System.out.println("There are " + resultIs + " punctuation marks in your text");
+    }
+}
