e8962d7cfd33a4765ce2f9c5190adc8518f70d1c
Tzeentche <ilyasugako87@gmail.com>
2020-03-15 23:40:23 +0300

    Create Appliance Ancestor class for 24-th task.

---
+++ src/test24/ApplianceAncestor.java
@@ -0,0 +1,50 @@
+package test24;
+
+public class ApplianceAncestor {
+
+    double length;
+    double width;
+    double weight;
+    String color;
+    int highLowVoltage;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getHighLowVoltage() {
+        return highLowVoltage;
+    }
+
+    public void setHighLowVoltage(int highLowVoltage) {
+        this.highLowVoltage = highLowVoltage;
+    }
+}
