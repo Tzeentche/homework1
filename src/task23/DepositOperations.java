e33f2b39583cd0305d8e49dce7b11242a99a2275
Tzeentche <ilyasugako87@gmail.com>
2020-03-15 23:39:30 +0300

    Refactored Depoit Operations class for 23-th task.

--- src/task23/DepositOperations.java
+++ src/task23/DepositOperations.java
@@ -1,14 +1,51 @@
 package task23;
 
+import java.util.*;
+
 public class DepositOperations {
 
-    private int deposite() {
+    Scanner scn = new Scanner(System.in);
+    public int usersDeposite = 0;
 
-        return 0;
+    public int getUsersDeposite() {
+        return usersDeposite;
     }
 
-    private int withdraw() {
+    private int billWorker() {
 
-        return 0;
+        while (true) {
+            System.out.println("Please, dial an amount, equals 20 or 50 or 100:");
+            if (scn == null) {
+                scn = new Scanner(System.in);
+
+                int usersMoney = 0;
+
+                try {
+                    usersMoney = scn.nextInt();
+                } catch (InputMismatchException ex) {
+                    System.out.println("Please, dial Integer value, equals 20 or 50 or 100!");
+                }
+
+                if (usersMoney == 20 || usersMoney == 50 || usersMoney == 100) {
+                    return usersMoney;
+
+                } else {
+                    System.out.println("Please, dial Integer value, equals 20 or 50 or 100:");
+                }
+            }
+        }
+    }
+
+    public int depositeMoney() {
+        int depositIncrement = billWorker();
+        return usersDeposite += depositIncrement;
+    }
+
+    public int withdrawMoney() {
+        int depositIncrement = billWorker();
+        if(depositIncrement < usersDeposite){
+            return usersDeposite -= depositIncrement;
+        }
+        else return 0;
     }
 }
