b25158813dbf4450e94e35625a433c7622e948a6
Tzeentche <ilyasugako87@gmail.com>
2020-03-15 23:41:18 +0300

    Create Freezer class extends Refregerator class for 24-th task.

---
+++ src/test24/Freezer.java
@@ -0,0 +1,19 @@
+package test24;
+
+public class Freezer extends Refregerator {
+
+    double length;
+    double width;
+    double weight;
+    String color;
+    int highLowVoltage;
+
+    private Freezer() {
+
+        length = 50.00;
+        width = 50.00;
+        weight = 10.00;
+        color = "white";
+        highLowVoltage = 220;
+    }
+}
