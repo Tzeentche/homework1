180b02aecea5f1d22b2b71427526dce6e070961b
Tzeentche <ilyasugako87@gmail.com>
2020-03-15 23:40:50 +0300

    Create Refregerator class extends Appliance Ancestor class for 24-th task.

---
+++ src/test24/Refregerator.java
@@ -0,0 +1,18 @@
+package test24;
+
+public class Refregerator extends ApplianceAncestor {
+
+    double length;
+    double width;
+    double weight;
+    String color;
+    int highLowVoltage;
+
+    public Refregerator() {
+        length = 100.00;
+        width = 50.00;
+        weight = 40.00;
+        color = "grey";
+        highLowVoltage = 220;
+    }
+}
