2a176a4885c59b8d8c48ff54abcaa0eefb652711
Tzeentche <ilyasugako87@gmail.com>
2020-03-20 23:40:53 +0300

    Started to develop 26-th task.

---
+++ src/task26/Marks.java
@@ -0,0 +1,11 @@
+package task26;
+
+//      Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
+//      Удалить неудовлетворительные оценки из списка.
+
+public class Marks {
+
+    public static void main(String[] args) {
+
+    }
+}
