49ee91321a01f2d78d0d7d2c88e888d065a41990
Tzeentche <ilyasugako87@gmail.com>
2020-03-21 22:35:48 +0300

    Fixed errors in Task 2, refactored logic.

--- src/task2/DaysAndWeeks.java
+++ src/task2/DaysAndWeeks.java
@@ -9,24 +9,13 @@ public class DaysAndWeeks {
         int s = 506923;
         int sec, m, min, h, hours, d, days, w, weeks;
         sec = s % 60;
-        System.out.println(sec + " 40");
         m = (s - sec) / 60;
-        System.out.println(m + " 6166");
         min = m % 60;
-        System.out.println(min + " 46");
         h = (m - min) / 60;
-        System.out.println(h + " 102");
-
         hours = h % 24;
-        System.out.println(hours + " ??");
         d = (h - hours) / 24;
-        System.out.println(d + " ???");
         days = d % 7;
-        System.out.println(days + " ????");
         w = (d - days) / 7;
-        System.out.println(w + " ?????");
-//        weeks = w % 7;
-//        System.out.println(weeks + " ??????");
 
         System.out.println("Недель: " + w + " дней: " + days);
         System.out.println(" часов: " + hours + " минут: " + min + " секунд: " + sec);
