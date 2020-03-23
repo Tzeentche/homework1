8f6af5df418d570384a1509a8fdc64f66c7aba77
Tzeentche <ilyasugako87@gmail.com>
2020-03-24 00:00:36 +0300

    Refactored code to task 26.

--- src/task26/Marks.java
+++ src/task26/Marks.java
@@ -3,9 +3,13 @@ package task26;
 //      Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
 //      Удалить неудовлетворительные оценки из списка.
 
+import java.util.ArrayList;
+
 public class Marks {
 
     public static void main(String[] args) {
 
     }
+
+    ArrayList aList = new ArrayList<>();
 }
