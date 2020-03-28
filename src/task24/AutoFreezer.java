5bea3fceda574031405a23c9fe1ab023e2eed90f
Tzeentche <ilyasugako87@gmail.com>
2020-03-28 23:38:32 +0300

    Refactored class AutoFreezer.

---
+++ src/task24/AutoFreezer.java
@@ -0,0 +1,14 @@
+package task24;
+
+public class AutoFreezer extends Refregerator {
+
+    public AutoFreezer() {
+
+        setLength(30.00);
+        setWidth(30.00);
+        setWeight(5.00);
+        setColor("Green");
+        setHighLowVoltage(12);
+        off();
+    }
+}
