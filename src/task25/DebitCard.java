c48c2875928ea1c8b57113a0af085342d3fbbb2d
Tzeentche <ilyasugako87@gmail.com>
2020-03-29 01:03:47 +0300

    Add class DebitCard.

---
+++ src/task25/DebitCard.java
@@ -0,0 +1,13 @@
+package task25;
+
+public class DebitCard extends CardAncestor {
+
+    public DebitCard() {
+
+        setColor("Black");
+        setBank("Alfa-Bank");
+        setPremiumLevel("Gold");
+        setCurrency("USD");
+        setNamed(true);
+    }
+}
