7a949e0e54077ad43ea399f784226135f76da31f
Tzeentche <ilyasugako87@gmail.com>
2020-03-29 01:04:10 +0300

    Add class OverdraftCard.

---
+++ src/task25/OverdraftCard.java
@@ -0,0 +1,13 @@
+package task25;
+
+public class OverdraftCard extends DebitCard {
+
+    public OverdraftCard() {
+
+        setColor("Red");
+        setBank("MTBank");
+        setPremiumLevel("Simple");
+        setCurrency("BYN");
+        setNamed(false);
+    }
+}
