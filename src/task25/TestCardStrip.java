9ce3ce06dcde187e742a113f81d1610be20d83af
Tzeentche <ilyasugako87@gmail.com>
2020-03-29 01:06:44 +0300

    Create class TestCardStrip.

---
+++ src/task25/TestCardStrip.java
@@ -0,0 +1,14 @@
+package task25;
+
+public class TestCardStrip extends VirtualCard {
+
+    public TestCardStrip() {
+
+        setColor("No");
+        setBank("Strip");
+        setPremiumLevel("Simple");
+        setCurrency("USD");
+        setNamed(false);
+        visa();
+    }
+}
