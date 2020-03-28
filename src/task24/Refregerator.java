27fc8867595ffeffdeaf1968f252e69ca13646f1
Tzeentche <ilyasugako87@gmail.com>
2020-03-28 23:39:29 +0300

    Refactored class Refregerator.

--- src/task24/Refregerator.java
+++ src/task24/Refregerator.java
@@ -1,18 +1,17 @@
 package task24;
 
+//      Создать иерархию классов, описывающих бытовую технику. Создать несколько объектов описанных классов,
+//      часть из них включить в розетку. Иерархия должна иметь хотя бы три уровня.
+
 public class Refregerator extends ApplianceAncestor {
 
-    double length;
-    double width;
-    double weight;
-    String color;
-    int highLowVoltage;
-
     public Refregerator() {
-        length = 100.00;
-        width = 50.00;
-        weight = 40.00;
-        color = "grey";
-        highLowVoltage = 220;
+
+        setLength(100.00);
+        setWidth(50.00);
+        setWeight(40.00);
+        setColor("Gray");
+        setHighLowVoltage(220);
+        on();
     }
 }
