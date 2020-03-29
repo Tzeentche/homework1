71181d01a5df8271297b6b59443a1ead82166e02
Tzeentche <ilyasugako87@gmail.com>
2020-03-29 21:25:51 +0300

    Add RatingHandler class to task 26.

---
+++ src/task26/RatingHandler.java
@@ -0,0 +1,20 @@
+package task26;
+
+import java.util.ArrayList;
+
+public class RatingHandler {
+
+    ArrayList badMarksKilelr(int minMark, ArrayList marksStorage) {
+
+        for(int i = 0; i < marksStorage.size(); i++) {
+
+            for (int j = 0; j < marksStorage.size(); j++) {
+
+                if ((int) marksStorage.get(j) <= minMark) {
+                    marksStorage.remove(j);
+                }
+            }
+        }
+        return marksStorage;
+    }
+}
