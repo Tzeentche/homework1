4be56e6e26342c7dd3744133483bfe7e166b3d55
Tzeentche <ilyasugako87@gmail.com>
2020-03-29 21:26:20 +0300

    Add UsersDialog class to task 26.

---
+++ src/task26/UsersDialog.java
@@ -0,0 +1,39 @@
+package task26;
+
+import java.util.*;
+
+public class UsersDialog {
+
+    Scanner scn = new Scanner(System.in);
+    int studentsValue = 0;
+    int minMark = 0;
+
+    int sizeFinder() {
+        System.out.println("Please, enter Integer value (more than 0) of students in your group: ");
+        try {
+            studentsValue = scn.nextInt();
+            if(studentsValue <= 0) {
+                System.out.println("Unfortunately, You haven't enough students...");
+                System.exit(0);
+            }
+        } catch (InputMismatchException ex) {
+            System.out.println("You entered wrong value. Please, next time enter Integer value more, than 0.");
+        }
+        return studentsValue;
+    }
+
+    int minMarksLevel() {
+        System.out.println("Please, enter Integer value (more than 0 and less than 10) of minimum mark, that students in your group wll have: ");
+        try {
+            minMark = scn.nextInt();
+            if(minMark <= 0 | minMark > 10) {
+                System.out.println("Unfortunately, our education system operating with marks from 0 to 10.");
+                System.exit(0);
+            }
+            scn.close();
+        } catch (InputMismatchException ex) {
+            System.out.println("You entered wrong value. Please, next time enter Integer value more, than 0 and less than 10.");
+        }
+        return minMark;
+    }
+}
