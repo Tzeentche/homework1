42ac95b5c8a7ececa87e22540bec80c8bc56dd3a
Tzeentche <ilyasugako87@gmail.com>
2020-03-31 23:34:51 +0300

    Add MarksFinder Class to task 28.

--- src/task28/MarksFinder.java
+++ src/task28/MarksFinder.java
@@ -3,9 +3,19 @@ package task28;
 //      Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
 //      Найти самую высокую оценку с использованием итератора.
 
+import java.util.ArrayList;
+
 public class MarksFinder {
 
     public static void main(String[] args) {
 
+        UsersInput usersInput = new UsersInput();
+        ListContainer listContainer = new ListContainer();
+        ListHandler listHandler = new ListHandler();
+
+        int size = usersInput.listCreator();
+        ArrayList list = listContainer.listFiller(size);
+        System.out.println("Max mark is: ");
+        System.out.println(listHandler.maxMarkFinder(list));
     }
 }
