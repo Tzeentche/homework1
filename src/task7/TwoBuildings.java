3412af21f7e944577e9a85bf1cda0d3ddc94d718
Tzeentche <ilyasugako87@gmail.com>
2020-04-01 10:45:30 +0300

    Fixed errors in Task 7.

--- src/task7/TwoBuildings.java
+++ src/task7/TwoBuildings.java
@@ -7,38 +7,35 @@ import java.util.*;
 
 public class TwoBuildings {
 
-    int[] sizes = new int[6];
-    String[] counter = {"first", "second"};
-    Scanner scn = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         SquareBuilding build = new SquareBuilding();
-        int[] fristBuild = build.usersInputBuilding();
-
         SquareBuilding build2 = new SquareBuilding();
+        SquareBuilding squar = new SquareBuilding();
+
+        int[] fristBuild = build.usersInputBuilding();
         int[] secondBuild = build2.usersInputBuilding();
-
-        SquareBuilding build3 = new SquareBuilding();
-        int[] standMyGround = build3.usersInputSquare();
-
+        int[] standMyGround = squar.usersInputSquare();
         comparator(fristBuild, secondBuild, standMyGround);
     }
 
-    static void comparator(int[] home1, int[] home2, int[] groud) {
-
-                if((home1[0] * home1[1]) + (home2[0] * home2[1]) > (groud[0] * groud[1])) {
-
-                    System.out.println("Your houses is too big!");
-                } else {
-
-                    System.out.println("Everything is Ok!");
+    static void comparator(int[] home1, int[] home2, int[] ground) {
+                if(((ground[0] >= (home1[0] + home2[0])) && (ground[1] >= home1[1] && ground[1] >= home2[1])) |
+                        ((ground[0] >= (home1[0] + home2[1])) && (ground[1] >= home1[1] && ground[1] >= home2[0])) |
+                        ((ground[0] >= (home1[1] + home2[0])) && (ground[1] >= home1[0] && ground[1] >= home2[1])) |
+                        ((ground[0] >= (home1[1] + home2[1])) && (ground[1] >= home1[0] && ground[1] >= home2[0])) |
+                        ((ground[1] >= (home1[0] + home2[0])) && (ground[0] >= home1[1] && ground[0] >= home2[1])) |
+                        ((ground[1] >= (home1[0] + home2[1])) && (ground[0] >= home1[1] && ground[0] >= home2[0])) |
+                        ((ground[1] >= (home1[1] + home2[0])) && (ground[0] >= home1[0] && ground[0] >= home2[1])) |
+                        ((ground[1] >= (home1[1] + home2[1])) && (ground[0] >= home1[0] && ground[0] >= home2[0]))) {
+                        System.out.println("Everything is Ok!");
+                    } else {
+                        System.out.println("Your houses is too big!");
                 }
     }
 }
 
 class SquareBuilding {
-
     String[] sides = {"First", "Second"};
     int[] sizes = new int[2];
     Scanner scn = new Scanner(System.in);
@@ -46,14 +43,12 @@ class SquareBuilding {
     int[] usersInputBuilding() {
 
         for(int i = 0; i < sides.length; i++) {
-
             System.out.println("Please, enter " + sides[i] + " size for your building:");
 
             try {
                 sizes[i] = scn.nextInt();
 
             } catch (InputMismatchException ex) {
-
                 System.out.println("You entered not integer value. Next time be carefully, please!");
             }
         }
@@ -61,21 +56,17 @@ class SquareBuilding {
     }
 
     int[] usersInputSquare() {
-
         for(int i = 0; i < sides.length; i++) {
-
             System.out.println("Please, enter " + sides[i] + " size for your ground:");
 
             try {
                 sizes[i] = scn.nextInt();
 
             } catch (InputMismatchException ex) {
-
                 System.out.println("You entered not integer value. Next time be carefully, please!");
             }
         }
         scn.close();
-
         return sizes;
     }
-        }
+}
