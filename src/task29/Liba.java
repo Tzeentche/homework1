5af6c9d18d0a32289cd786740420075010bc08d6
Tzeentche <ilyasugako87@gmail.com>
2020-04-02 23:39:19 +0300

    Started to developing code for task 29.

--- src/task29/Liba.java
+++ src/task29/Liba.java
@@ -2,9 +2,52 @@ package task29;
 
 //      Имеется текст. Следует составить для него частотный словарь.
 
+import java.text.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class Liba {
 
-    public static void main(String[] args) {
+    static HashMap<String, Integer> vocabulary = new HashMap<String, Integer>();
+    static Scanner sc = new Scanner(System.in);
 
+    public static boolean isEngLetter(char c){
+        return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
+    }
+
+    public static boolean isWord(String s){
+        for(int i = 0; i < s.length(); i++){
+            if(!(isEngLetter(s.charAt(i)) ||
+                    Character.isDigit(s.charAt(i)) ||
+                    Pattern.matches("\\p{Punct}", s))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String toAlpha(String s){
+        return s.replaceAll("[^a-zA-Z]","");
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+            while(sc.hasNext()){
+                String s = sc.next();
+                if(isWord(s)){
+                    s = toAlpha(s);
+                    if(!s.isEmpty()){
+                        vocabulary.put(s, vocabulary.containsKey(s) ? vocabulary.get(s) + 1 : 1);
+                    }
+                }
+            }
+            int sum = 0;
+            for(int value : vocabulary.values()){
+                sum += value;
+            }
+            NumberFormat formatter = new DecimalFormat("#0.0000");
+
+            for(String s : vocabulary.keySet()){
+                System.out.println(s + " " + formatter.format((double)vocabulary.get(s)/sum));
+            }
     }
 }
