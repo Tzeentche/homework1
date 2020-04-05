98e9709540dd2cc50363ab20341b2b1200967e49
Tzeentche <ilyasugako87@gmail.com>
2020-04-05 23:00:20 +0300

    Refactored class Main to class 30.

---
+++ src/task30/Main.java
@@ -0,0 +1,12 @@
+package task30;
+
+//      Создать файл с текстом, прочитать, подсчитать в тексте количество знаков препинания и слов.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FileConsists fileConsists = new FileConsists();
+        fileConsists.readFromFile();
+    }
+}
