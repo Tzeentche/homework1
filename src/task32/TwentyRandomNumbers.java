a224b5ecfab2bd1c15769f3dc6ae866ccd1d8312
Tzeentche <ilyasugako87@gmail.com>
2020-04-05 23:12:09 +0300

    Refactored class TwentyRandomNumbers to task 32.

--- src/task32/TwentyRandomNumbers.java
+++ src/task32/TwentyRandomNumbers.java
@@ -1,4 +1,10 @@
 package task32;
 
+//      Записать в двоичный файл 20 случайных чисел. Прочитать записанный файл, распечатать числа и их среднее арифметическое.
+
 public class TwentyRandomNumbers {
+
+    public static void main(String[] args) {
+
+    }
 }
