a6ef8ed79f838d391854e2c9cc939f14ae39d672
Tzeentche <ilyasugako87@gmail.com>
2020-04-05 23:07:13 +0300

    Refactored class NumbersConsists to task 31.

--- src/task31/NumbersConsists.java
+++ src/task31/NumbersConsists.java
@@ -1,4 +1,15 @@
 package task31;
 
+//      Имеется файл с текстом, в котором присутствуют числа. Нати все числа, распечатать, посчитать сумму,
+//      убрать все повторяющиеся числа и снова распечатать.
+
 public class NumbersConsists {
+
+    public static void main(String[] args) {
+
+        FileReader fileReader = new FileReader();
+
+        fileReader.readFromFile();
+    }
+
 }
