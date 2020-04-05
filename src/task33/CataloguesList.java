1fbbc8ab65f2c3f5ca8891e19bff30ee857d6770
Tzeentche <ilyasugako87@gmail.com>
2020-04-05 23:20:04 +0300

    Refactored class CatalogueList to task 33.

--- src/task33/CataloguesList.java
+++ src/task33/CataloguesList.java
@@ -1,4 +1,10 @@
 package task33;
 
+//      Вывести список файлов и каталогов выбранного каталога на диске. Файлы и каталоги должны быть распечатаны отдельно.
+
 public class CataloguesList {
+
+    public static void main(String[] args) {
+
+    }
 }
