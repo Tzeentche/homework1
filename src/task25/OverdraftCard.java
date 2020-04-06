a6bc21dd02a4f785daf2961623a65161b98ec23f
Tzeentche <ilyasugako87@gmail.com>
2020-04-07 00:15:28 +0300

    Refactored code in class OverdraftCard in task 25.

--- src/task25/OverdraftCard.java
+++ src/task25/OverdraftCard.java
@@ -2,13 +2,23 @@ package task25;
 
 public class OverdraftCard extends DebitCard {
 
+    boolean isNFC;
+
     public OverdraftCard() {
 
         setColor("Red");
         setBank("MTBank");
-        setPremiumLevel("Simple");
         setCurrency("BYN");
         setNamed(false);
         visa();
+        setIsNFC(true);
+    }
+
+    public boolean getIsNFC() {
+        return isNFC;
+    }
+
+    public void setIsNFC(boolean NFC) {
+        isNFC = NFC;
     }
 }
