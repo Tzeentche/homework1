ff02792c9236ae616ae51c33e9ea2366d1b93c40
Tzeentche <ilyasugako87@gmail.com>
2020-04-07 00:16:00 +0300

    Refactored code in class VirtualCard in task 25.

--- src/task25/VirtualCard.java
+++ src/task25/VirtualCard.java
@@ -2,13 +2,24 @@ package task25;
 
 public class VirtualCard extends OverdraftCard {
 
+    boolean isApplePay;
+
     public VirtualCard() {
 
         setColor("Green");
         setBank("MTBank");
-        setPremiumLevel("Black");
         setCurrency("BIT");
         setNamed(false);
+        setIsNFC(false);
+        setIsApplePay(true);
         mastercard();
+    }
+
+    public boolean getIsApplePay() {
+        return isApplePay;
+    }
+
+    public void setIsApplePay(boolean isApplePay) {
+        isApplePay = isApplePay;
     }
 }
