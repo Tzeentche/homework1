4e88b09f4fb700bd93a695faf5f4a2a2c9329590
Tzeentche <ilyasugako87@gmail.com>
2020-04-07 00:15:45 +0300

    Refactored code in class TestCardStrip in task 25.

--- src/task25/TestCardStrip.java
+++ src/task25/TestCardStrip.java
@@ -2,13 +2,26 @@ package task25;
 
 public class TestCardStrip extends VirtualCard {
 
+    boolean isOpenSource;
+
     public TestCardStrip() {
 
         setColor("No");
         setBank("Strip");
-        setPremiumLevel("Simple");
         setCurrency("USD");
         setNamed(false);
+        setIsNFC(false);
+        setIsApplePay(false);
         visa();
+        setIsOpenSource(true);
     }
+
+    public boolean getIsOpenSource() {
+        return isOpenSource;
+    }
+
+    public void setIsOpenSource(boolean openSource) {
+        isOpenSource = openSource;
+    }
+
 }
