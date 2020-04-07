9357425dee56910f2f2b4217cffe09ab15dbde3c
Tzeentche <ilyasugako87@gmail.com>
2020-04-08 00:38:44 +0300

    Refactored code in class Main to task 30.

--- src/task30/Main.java
+++ src/task30/Main.java
@@ -2,11 +2,23 @@ package task30;
 
 //      Создать файл с текстом, прочитать, подсчитать в тексте количество знаков препинания и слов.
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        FileConsists fileConsists = new FileConsists();
-        fileConsists.readFromFile();
+        NewFile newFile = new NewFile();
+        ReadFile readFile = new ReadFile();
+        Calculator calculator = new Calculator();
+
+        File textFile = new File("Text.txt");
+
+        newFile.createFile(textFile);
+
+        String textFromFile = readFile.readFromFile(textFile);
+        System.out.println(textFromFile);
+        System.out.println("Punctuations value is: " + calculator.dotsCounter(textFromFile));
+        System.out.println("Words value is: " + calculator.wordsCounter(textFromFile));
     }
 }
