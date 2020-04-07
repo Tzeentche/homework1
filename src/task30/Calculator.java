95ed0cdc52edfb6f675f6959e669f1e69382af0c
Tzeentche <ilyasugako87@gmail.com>
2020-04-08 00:38:16 +0300

    Refactored code in class Calculator to task 30.

--- src/task30/Calculator.java
+++ src/task30/Calculator.java
@@ -4,5 +4,18 @@ package task30;
 
 public class Calculator {
 
+    int dotsCounter(String text) {
+        String punct = text.replaceAll("[^\\p{P}]", "");
+        if (!punct.isBlank()) {
+            return punct.length();
+        } else return 0;
+    }
 
+    int wordsCounter(String text) {
+        String noPunct = text.replaceAll("[^A-Za-zА-Яа-я \\s]", "");
+        String[] words = noPunct.split("\\s+");
+        if (!noPunct.isBlank()) {
+            return words.length;
+        } else return 0;
+    }
 }
