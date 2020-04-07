3992b4529af9551ca12cd0f8069a7e4a32352317
Tzeentche <ilyasugako87@gmail.com>
2020-04-08 01:16:36 +0300

    Add class NumbersCalculator to task 31.

--- src/task31/NumbersCalculator.java
+++ src/task31/NumbersCalculator.java
@@ -1,8 +1,30 @@
 package task31;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class NumbersCalculator {
 
-    void calculate() {
+    List<String> numbersFinder(String text) {
+        String numbers = text.replaceAll("[^\\d\\s]", "");
 
+        if (!numbers.isBlank()) {
+            return Stream.of(numbers.split("\\s+"))
+                    .filter(s -> !s.isBlank()).map(String::new)
+                    .collect(Collectors.toList());
+        } else return new ArrayList<>();
+    }
+
+    int counter(List<String> numbers) {
+
+        if (!numbers.isEmpty()) {
+            return numbers.stream()
+                    .mapToInt(Integer::parseInt)
+                    .reduce(0, (a, b) -> (a + b));
+        } else return 0;
+    }
+
+    List<String> repeatKiller(List<String> numbers) {
+        return numbers.stream().distinct().collect(Collectors.toList());
     }
 }
