d0d656328d4dd692e9055db70830cfb69e5b0f9c
Tzeentche <ilyasugako87@gmail.com>
2020-04-08 01:17:00 +0300

    Add class NumbersConsists to task 31.

--- src/task31/NumbersConsists.java
+++ src/task31/NumbersConsists.java
@@ -3,13 +3,22 @@ package task31;
 //      Имеется файл с текстом, в котором присутствуют числа. Нати все числа, распечатать, посчитать сумму,
 //      убрать все повторяющиеся числа и снова распечатать.
 
+import java.io.*;
+import java.util.*;
+
 public class NumbersConsists {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        FileReader fileReader = new FileReader();
+        File textFile = new File("Text.txt");
+        ReadTextFromFile readTextFromFile = new ReadTextFromFile();
+        NumbersCalculator numbersCalculator = new NumbersCalculator();
+        String text = readTextFromFile.readText(textFile);
+        List<String> numbers;
+        numbers = numbersCalculator.numbersFinder(text);
 
-        fileReader.readFromFile();
+        System.out.println("In our text we have next numbers: " + numbers.toString());
+        System.out.println("If we plus this numbers, we will have next result: " + numbersCalculator.counter(numbers));
+        System.out.println("Most of all, we have next unique numbers: " + numbersCalculator.repeatKiller(numbers));
     }
-
 }
