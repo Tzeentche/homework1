691b6525c616ad25387cdab12a5f8171807b4a46
Tzeentche <ilyasugako87@gmail.com>
2020-04-08 00:22:21 +0300

    Refactored code in class Liba to task 29.

--- src/task29/Liba.java
+++ src/task29/Liba.java
@@ -2,52 +2,27 @@ package task29;
 
 //      Имеется текст. Следует составить для него частотный словарь.
 
-import java.text.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class Liba {
 
-    static HashMap<String, Integer> vocabulary = new HashMap<String, Integer>();
-    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
 
-    public static boolean isEngLetter(char c){
-        return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
-    }
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\JAVA\\Projects\\homework1\\src\\task29\\Text.txt"));
+        Map<String, Integer> wordsMap = new HashMap<>();
 
-    public static boolean isWord(String s){
-        for(int i = 0; i < s.length(); i++){
-            if(!(isEngLetter(s.charAt(i)) ||
-                    Character.isDigit(s.charAt(i)) ||
-                    Pattern.matches("\\p{Punct}", s))) {
-                return false;
+        while ((line = reader. readLine()) != null) {
+            String[] words = line.replaceAll("\\p{Punct}", "").split(" ");
+            List<String> wordsList = new ArrayList<>(Arrays.asList(words));
+
+            for (String word : words) {
+                wordsMap.put(word, Collections.frequency(wordsList, word));
             }
+
+            System.out.println("We have next result: " + wordsMap);
         }
-        return true;
-    }
-
-    public static String toAlpha(String s){
-        return s.replaceAll("[^a-zA-Z]","");
-    }
-
-    public static void main(String[] args) throws java.lang.Exception {
-            while(sc.hasNext()){
-                String s = sc.next();
-                if(isWord(s)){
-                    s = toAlpha(s);
-                    if(!s.isEmpty()){
-                        vocabulary.put(s, vocabulary.containsKey(s) ? vocabulary.get(s) + 1 : 1);
-                    }
-                }
-            }
-            int sum = 0;
-            for(int value : vocabulary.values()){
-                sum += value;
-            }
-            NumberFormat formatter = new DecimalFormat("#0.0000");
-
-            for(String s : vocabulary.keySet()){
-                System.out.println(s + " " + formatter.format((double)vocabulary.get(s)/sum));
-            }
     }
 }
